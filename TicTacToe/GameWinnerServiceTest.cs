71c559cf84e2b4b04a997743c59708f613f25725
Lasse Jacobsen <lassejacobsen@mail.com>
2016-09-29 11:36:30 +0200

    Refactoring

--- TicTacToe/GameWinnerServiceTest.cs
+++ TicTacToe/GameWinnerServiceTest.cs
@@ -7,30 +7,11 @@ namespace TicTacToe
     [TestClass]
     public class GameWinnerServiceTest
     {
-            IGameWinnerService gameWinnerService = new GameWinnerService();
-        char[,] gameBoard = new char[3, 3] { {' ', ' ', ' '},
-                                             {' ', ' ', ' '},
-                                             {' ', ' ', ' '}};
+        IGameWinnerService gameWinnerService = new GameWinnerService();
+        private char[,] gameBoard = new char[3, 3] { {' ', ' ', ' '},
+                                                     {' ', ' ', ' '},
+                                                     {' ', ' ', ' '}};
 
-        [TestMethod]
-        public void NeitherPlayerHasThreeInARow()
-        {            
-            const char expected = ' ';
-            var actual = gameWinnerService.Validate(gameBoard);
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void PlayerWithAllSpacesInTopRowIsWinning()
-        {      
-            const char expected = 'X';
-            var gameBoard = new char[3, 3]
-                    { { expected, expected, expected},
-                      {' ', ' ', ' '},
-                      {' ', ' ', ' '}};
-
-            var actual = gameWinnerService.Validate(gameBoard);
-            Assert.AreEqual(expected.ToString(), actual.ToString());
-        }
         [TestMethod]
         public void PlayerWithAllSpacesInColumIsWinner()
         {
@@ -41,6 +22,29 @@ namespace TicTacToe
             {
                 _gameBoard[columIndex, 0] = expected;
             }
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [TestMethod]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PlayerWithAllSpacesInTopRowIsWinning()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]
+                    { { expected, expected, expected},
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '}
+                    };
+
             var actual = gameWinnerService.Validate(gameBoard);
             Assert.AreEqual(expected.ToString(), actual.ToString());
         }
