19818ade615abf26f926093e0e49db7870e65c40
chrisrock2606 <mail@christian-web.dk>
2016-09-29 10:10:31 +0200

    Refactoring
    Refactoring

--- TicTacToe/GameWinnerServiceTest.cs
+++ TicTacToe/GameWinnerServiceTest.cs
@@ -7,32 +7,33 @@ namespace TicTacToe
     [TestClass]
     public class GameWinnerServiceTest
     {
+        IGameWinnerService gameWinnerService = new GameWinnerService();
+
+        char[,] gameBoard = new char[3, 3] { {' ', ' ', ' '},
+                                             {' ', ' ', ' '},
+                                             {' ', ' ', ' '}};
+
+
         [TestMethod]
         public void NeitherPlayerHasThreeInARow()
         {
-            IGameWinnerService gameWinnerService = new GameWinnerService();
             const char expected = ' ';
-            var gameBoard = new char[3, 3] {{' ', ' ', ' '},
-                                            {' ', ' ', ' '},
-                                            {' ', ' ', ' '}};
-
             var actual = gameWinnerService.Validate(gameBoard);
             Assert.AreEqual(expected, actual);
         }
 
+
         [TestMethod]
         public void PlayerWithAllSpacesInTopRowIsWinning()
         {
-            IGameWinnerService gameWinnerService = new GameWinnerService();
             const char expected = 'X';
             var gameBoard = new char[3, 3]
                     { { expected, expected, expected},
-                      {' ', ' ', ' '},
-                      {' ', ' ', ' '}};
+            {' ', ' ', ' '},
+          {' ', ' ', ' '}};
 
             var actual = gameWinnerService.Validate(gameBoard);
             Assert.AreEqual(expected.ToString(), actual.ToString());
         }
-
     }
 }
