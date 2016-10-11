d4088350a507fdfd39e1c111913ed268b2ad17ce
black <black@DESKTOP-IAP7KVH>
2016-10-11 14:19:09 +0200

    Column and diagonal

--- AI/AI.cs
+++ AI/AI.cs
@@ -13,6 +13,10 @@ namespace TicTacToe.AI
         public char[,] NextMove(char[,] gameBoard)
         {
             gameBoard = CanWinOnRow(gameBoard);
+            
+            gameBoard = CanWinOnColumn(gameBoard);
+
+            gameBoard = CanWinDiagonally(gameBoard);
 
             return gameBoard;
         }
@@ -21,29 +25,64 @@ namespace TicTacToe.AI
         {
         	for(int i = 0; i <= 2; i++)
             {
-        		var columnOneChar   = gameBoard[i, 0];
-				var columnTwoChar   = gameBoard[i, 1];
-        		var columnThreeChar = gameBoard[i, 2];
+          		int winField = WinField(gameBoard[i, 0], gameBoard[i, 1], gameBoard[i, 2]);
                 
-                if (columnOneChar == SymbolForNoWinner && columnTwoChar == columnThreeChar)
-                {
-                    gameBoard[i,0] = 'X';
-                }
-
-                if (columnTwoChar == SymbolForNoWinner && columnThreeChar == columnOneChar)
-                {
-                    gameBoard[i,1] = 'X';
-                }
-
-                if (columnThreeChar == SymbolForNoWinner && columnTwoChar == columnOneChar)
-                {
-                    gameBoard[i,2] = 'X';
-                }
+                if(winField >= 0)
+              		gameBoard[i,winField] = 'X';
             }
 
             return gameBoard;
         }
-        
+
+        public char[,] CanWinOnColumn(char[,] gameBoard)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                int winField = WinField(gameBoard[0, i], gameBoard[1, i], gameBoard[2, i]);
+
+                if (winField >= 0)
+                    gameBoard[winField, i] = 'X';
+
+            }
+
+            return gameBoard;
+        }
+
+        private int WinField(char a, char b, char c)
+        {
+            if (a == SymbolForNoWinner && b == c && c != SymbolForNoWinner)
+                return 0;
+            if (b == SymbolForNoWinner && a == c && a != SymbolForNoWinner)
+                return 1;
+            if (c == SymbolForNoWinner && a == b && a != SymbolForNoWinner) 
+                return 2;
+                
+            return -1;
+        }
+
+        public char[,] CanWinDiagonally(char[,] gameBoard)
+        {
+            int winRightToLeft = WinField(gameBoard[0,2], gameBoard[1,1], gameBoard[2,0]);
+            int winLeftToRight = WinField(gameBoard[0,0], gameBoard[1,1], gameBoard[2,2]);
+            
+            if(winLeftToRight >= 0)
+                    gameBoard[winLeftToRight,winLeftToRight] = 'X';
+                
+            switch(winRightToLeft)
+            {
+                    case 0:
+                        gameBoard[0,2] = 'X';
+                        break;
+                case 1:
+                        gameBoard[1,1] = 'X';
+                        break;
+                case 2:
+                        gameBoard[2,0] = 'X';
+                        break;
+            }
+            
+            return gameBoard;
+        }
 
     }
 }
--- TicTacToe/AITest.cs
+++ TicTacToe/AITest.cs
@@ -9,32 +9,277 @@ using TicTacToe.AI;
 namespace TicTacToe
 {
     [TestClass]
-    class AITest
+    public class AITest
     {
+        AIOpponent ai = new AIOpponent();
+
         [TestMethod]
-        public void AICanWinTopRowLeft()
+        public void AICanWinFirstColumn()
         {
-            AIOpponent ai       = new AIOpponent();
+            const char expected = 'X';
+            var gameBoardBefore = new char[3, 3]
+                    { { ' ',' ' , ' '},
+                      {expected, ' ', ' '},
+                      {expected, ' ', ' '}
+                    };
+
+            var actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[0, 0].ToString());
+
+
+
+            gameBoardBefore = new char[3, 3]
+                    { {expected, ' ' , ' '},
+                      {' ', ' ', ' '},
+                      {expected, ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[1, 0].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { { expected, ' ' , ' '},
+                      {expected, ' ', ' '},
+                      {' ', ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[2, 0].ToString());
+        }
+
+        [TestMethod]
+        public void AICanWinSecondColumn()
+        {
+            const char expected = 'X';
+            var gameBoardBefore = new char[3, 3]
+                    { { ' ',' ' , ' '},
+                      {' ', expected, ' '},
+                      {' ', expected, ' '}
+                    };
+
+            var actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[0, 1].ToString());
+
+
+
+            gameBoardBefore = new char[3, 3]
+                    { {' ', expected , ' '},
+                      {' ', ' ', ' '},
+                      {' ', expected, ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[1, 1].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { {' ', expected, ' '},
+                      {' ', expected, ' '},
+                      {' ', ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[2, 1].ToString());
+        }
+
+        [TestMethod]
+        public void AICanWinThirdColumn()
+        {
+            const char expected = 'X';
+            var gameBoardBefore = new char[3, 3]
+                    { { ' ',' ' , ' '},
+                      {' ', ' ', expected},
+                      {' ', ' ', expected}
+                    };
+
+            var actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[0, 2].ToString());
+
+
+
+            gameBoardBefore = new char[3, 3]
+                    { {' ', ' ' , expected},
+                      {' ', ' ', ' '},
+                      {' ', ' ', expected}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[1, 2].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { {' ', ' ', expected},
+                      {' ', ' ', expected},
+                      {' ', ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[2, 2].ToString());
+        }
+
+        [TestMethod]
+        public void AICanWinTopRow()
+        {
             const char expected = 'X';
             var gameBoardBefore = new char[3, 3]
                     { { ' ', expected, expected},
                       {' ', ' ', ' '},
                       {' ', ' ', ' '}
                     };
-                    
-            var gameBoardAfter = new char[3, 3]
-                    { { expected, expected, expected},
+
+            var actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[0, 0].ToString());
+
+
+
+            gameBoardBefore = new char[3, 3]
+                    { { expected, ' ' , expected},
                       {' ', ' ', ' '},
                       {' ', ' ', ' '}
                     };
 
-            var actual = ai.NextMove(gameBoardBefore);
-            
-            Assert.AreEqual(gameBoardAfter.ToString(), actual.ToString());
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[0, 1].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { { expected, expected , ' '},
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[0, 2].ToString());
         }
 
+        [TestMethod]
+        public void AICanWinBottomRow()
+        {
+            const char expected = 'X';
+            var gameBoardBefore = new char[3, 3]
+                    { { ' ', ' ', ' '},
+                      { ' ', ' ', ' '},
+                      {' ', expected, expected}
+                    };
+
+            var actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[2, 0].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { { ' ', ' ', ' '},
+                      { ' ', ' ', ' '},
+                      {expected, ' ', expected}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[2, 1].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { { ' ', ' ', ' '},
+                      { ' ', ' ', ' '},
+                      {expected, expected, ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[2, 2].ToString());
+        }
+
+        [TestMethod]
+        public void AICanWinMiddleRow()
+        {
+            const char expected = 'X';
+            var gameBoardBefore = new char[3, 3]
+                    { { ' ', ' ' , ' '},
+                      {' ', expected, expected},
+                      {' ', ' ', ' '}
+                    };
+
+            var actual = ai.NextMove(gameBoardBefore);
+            Assert.AreEqual(expected.ToString(), actual[1, 0].ToString());
 
 
+            gameBoardBefore = new char[3, 3]
+                    { {' ', ' ' , ' '},
+                      {expected, ' ',expected},
+                      {' ', ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+
+            Assert.AreEqual(expected.ToString(), actual[1, 1].ToString());
+
+            gameBoardBefore = new char[3, 3]
+                    { { ' ', ' ' , ' '},
+                      {expected, expected, ' '},
+                      {' ', ' ', ' '}
+                    };
+
+            actual = ai.NextMove(gameBoardBefore);
+
+            Assert.AreEqual(expected.ToString(), actual[1, 2].ToString());
+        }
+
+        [TestMethod]
+        public void AICanWinDiagonallyLeftToRight()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]
+                        { {expected, ' ', ' '},
+                          {' ', expected, ' '},
+                          { ' ', ' ', ' '}
+                };
+
+            var actual = ai.NextMove(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual[2,2].ToString());
+
+            gameBoard = new char[3, 3]
+                        { {expected, ' ', ' '},
+                          {' ', ' ', ' '},
+                          { ' ', ' ', expected}
+                };
+
+            actual = ai.NextMove(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual[1,1].ToString());
+
+            gameBoard = new char[3, 3]
+                            { {' ', ' ', ' '},
+                          {' ', expected, ' '},
+                          { ' ', ' ', expected}
+                };
+
+            actual = ai.NextMove(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual[0,0].ToString());
+        }
+
+        [TestMethod]
+        public void AICanWinDiagonallyRightToLeft()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]
+                        { {' ', ' ', expected},
+                          {' ', expected, ' '},
+                          { ' ', ' ', ' '}
+                };
+
+            var actual = ai.NextMove(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual[2,0].ToString());
+
+            gameBoard = new char[3, 3]
+                        { {' ', ' ', expected},
+                          {' ', ' ', ' '},
+                          { expected, ' ', ' '}
+                };
+
+            actual = ai.NextMove(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual[1,1].ToString());
+
+            gameBoard = new char[3, 3]
+                            { {' ', ' ', ' '},
+                          {' ', expected, ' '},
+                          { expected, ' ', ' '}
+                };
+
+            actual = ai.NextMove(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual[0,2].ToString());
+
+        }
 
 
 
